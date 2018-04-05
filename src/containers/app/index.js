4ee1c51a8f5ea093f4971a6a2719ffb287e5eb2e
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2018-04-05 14:17:42 +0200

    chore: initial commit

---
+++ src/containers/app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Link } from 'react-router-dom'
+import Home from '../home'
+
+const App = () => (
+  <div>
+    <header>
+      <Link to="/">Home</Link>
+    </header>
+
+    <main>
+      <Route exact path="/" component={Home} />
+    </main>
+  </div>
+)
+
+export default App
