e7313552634d2a76b8377ceaea0232b4fb2d7b6a
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2018-04-11 15:52:08 +0200

    fix: reading headers, pubg

--- src/containers/home/components/spotlights/index.js
+++ src/containers/home/components/spotlights/index.js
@@ -6,13 +6,13 @@ const Spotlights = () => (
   <div className="a-spotlights">
     <h2>LES QUATRES JEUX MYTHIQUES DE RETOUR À L’UTT ARENA 2018.</h2>
     <p>
-      League of Legends, Counter-Strike: Global Offensive, Hearthstone et Overwatch. Les quatres jeux mythiques sont de retour pour cette édition 2018.<br/>
+      League of Legends, Counter-Strike: Global Offensive, Hearthstone et PUBG. Les quatres jeux mythiques sont de retour pour cette édition 2018.<br/>
       <br/>
       <strong>Répartition des cashprizes</strong> : to be announced<br/>
       <strong>Nombre de places par tournoi :</strong><br/>
         <strong>- Hearthstone :</strong> 32 slots<br/>
         <strong>- CS:GO :</strong> 8 slots<br/>
-        <strong>- Overwatch :</strong> 16 slots<br/>
+        <strong>- PUBG :</strong> 16 slots<br/>
         <strong>- League of Legends :</strong> 32 slots<br/>
     </p>
   </div>
