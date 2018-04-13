f4fc471aa53b57e9dcf0bbbd0c943e80580a4c58
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2018-04-13 22:19:41 +0200

    feat: konami

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 import Home from '../home'
+import asyncComponent from '../../components/async'
 
+const AsyncPizza = asyncComponent(() => import('../../components/pizza'));
 
 const App = () => (
   <div>
+    <AsyncPizza />
     <Route exact path={process.env.REACT_APP_BASEURL + '/'} component={Home} />
   </div>
 )
