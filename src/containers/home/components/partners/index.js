654fe9a6d4285ff9e7fcdf8a5f8abd939bc9af52
Arnaud9145 <arnaud.dufour@utt.fr>
2018-09-17 19:26:55 +0200

    added more partners

--- src/containers/home/components/partners/index.js
+++ src/containers/home/components/partners/index.js
@@ -4,18 +4,24 @@ import './partners.css'
 
 class Partners extends React.Component {
   render() {
+    let id = 1
     const partners = process.env.REACT_APP_PARTNERS.split(',').map(partner => ({
+      key: id++,
       name: partner,
       image: `${process.env.PUBLIC_URL}/${partner}.png`,
       url: process.env[`REACT_APP_PARTNER_${partner.toUpperCase()}_LINK`]
     }))
-    
     return (
       <div className="a-partners">
         <h2 className="a-partners__partner">PARTENAIRES</h2>
         <div className="a-partners_logos">
-          {partners.map((partner, i) => 
-            <a className="a-partners__images" href={partner.url}>
+          {partners.map((partner, i) =>
+            
+            <a
+              key={i}
+              className="a-partners__images"
+              href={partner.url}
+            >
               <img src={partner.image} key={i} alt={partner.name} />
             </a>
           )}
