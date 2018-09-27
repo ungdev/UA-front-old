7102502d55743a28593b5f165f3973b2b8acae4e
Arnaud9145 <arnaud.dufour@utt.fr>
2018-09-27 09:15:42 +0200

    fixed info text

--- src/containers/home/components/informations/index.js
+++ src/containers/home/components/informations/index.js
@@ -8,10 +8,10 @@ const Informations = () => (
     <p>
       L'UTT Arena revient pour sa 16ème édition les <span>7, 8 et 9 décembre 2018</span>. Cette édition aura
       lieu comme l'année dernière dans le cadre du <span>Festival des Jeux</span> au Centre d'exposition de Troyes, le Cube. Au programme
-        <span> 5 tournois</span> sur des incontournables de l'E-sports, du skill, des personnalités et des
-      rencontres, de nombreuses animations, des <span>lots</span> et du <span>cashprice</span> à gagner, qui rendront cette édition plus intense
+        <span> 5 tournois</span> sur des incontournables de l'E-sport, du skill, des personnalités et des
+      rencontres, de nombreuses animations, des <span>lots</span> et du <span>cashprize</span> à gagner, qui rendront cette édition plus intense
       et vibrante que jamais. Pas fan des jeux proposés ? Pas envie d’être dans un cadre compétitif
-      ? L’UTT Arena propose un <span>tounoi libre</span>, composé des jeux que vous choisissez ! Alors récupère ta
+      ? L’UTT Arena propose un <span>tournoi libre</span>, composé des jeux que vous choisissez ! Alors récupère ta
       souris, ton casque et ton câble réseau, branche ton écran et <span>vient imposer la domination de
       ton équipe dans le Cube</span>.
     </p>
