5a73b11423b8c631570dc8a495a998239dbd89e0
Arnaud9145 <arnaud.dufour@utt.fr>
2018-10-06 15:13:16 +0200

    added warnings when team is incomplete, spotlight is full etc

--- src/lib/teamStatus.js
+++ src/lib/teamStatus.js
@@ -2,16 +2,16 @@ export default state => {
   const team = state.user.user.team
 
   if (!team) {
-    return { status: 'Aucune équipe — Libre', theme: 'warning' }
+    return { status: 'Aucune équipe — Libre', theme: 'warning', complete: -1 }
   }
 
   const teamUsers = team.users
   const spotlight = team.spotlight
 
   if (team && !spotlight) {
-    return { status: 'Équipe non inscrite — Libre', theme: 'error' }
+    return { status: 'Équipe non inscrite — Libre', theme: 'error', complete: -1 }
   }
-  if(team && team.isInSpotlight) return { status: 'Équipe inscrite', theme: 'success' }
+  if(team && team.isInSpotlight) return { status: 'Équipe inscrite', theme: 'success', complete: 1 }
 
   const { isFull } = (state.spotlights.spotlights.find(s => s.id === state.user.user.team.spotlightId) || {})
 
@@ -21,15 +21,15 @@ export default state => {
 
   if (isFull) {
     if (playerCount < maxPlayers || playerPaidCount < maxPlayers) {
-      return { status: 'Équipe incomplète - tournoi plein', theme: 'error' }
+      return { status: 'Équipe incomplète - tournoi plein', theme: 'error', complete: 0 }
     } else {
-      return { status: 'Équipe complète — tournoi plein', theme: 'error' }
+      return { status: 'Équipe complète — tournoi plein', theme: 'error', complete: 1 }
     }
   } else {
     if (playerCount < maxPlayers || playerPaidCount < maxPlayers) {
-      return { status: 'Équipe incomplète', theme: 'warning' }
+      return { status: 'Équipe incomplète', theme: 'warning', complete: 0 }
     } else {
-      return { status: 'Équipe complète', theme: 'success' }
+      return { status: 'Équipe complète', theme: 'success', complete: 1 }
     }
   }
 
