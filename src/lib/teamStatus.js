67371eceead0169e97a4cad2be284b6b6903b763
Arnaud9145 <arnaud.dufour@utt.fr>
2018-10-06 14:15:21 +0200

    some change on team status

--- src/lib/teamStatus.js
+++ src/lib/teamStatus.js
@@ -11,20 +11,26 @@ export default state => {
   if (team && !spotlight) {
     return { status: 'Équipe non inscrite — Libre', theme: 'error' }
   }
+  if(team && team.isInSpotlight) return { status: 'Équipe inscrite', theme: 'success' }
 
   const { isFull } = (state.spotlights.spotlights.find(s => s.id === state.user.user.team.spotlightId) || {})
-
-  if (isFull) {
-    return { status: 'Équipe en retard — tournoi plein', theme: 'error' }
-  }
 
   const playerCount = teamUsers.size
   const playerPaidCount = teamUsers.filter(player => player.paid).length
   const maxPlayers = spotlight.perTeam
 
-  if (playerCount < maxPlayers || playerPaidCount < maxPlayers) {
-    return { status: 'Équipe incomplète', theme: 'warning' }
+  if (isFull) {
+    if (playerCount < maxPlayers || playerPaidCount < maxPlayers) {
+      return { status: 'Équipe incomplète - tournoi plein', theme: 'error' }
+    } else {
+      return { status: 'Équipe complète — tournoi plein', theme: 'error' }
+    }
+  } else {
+    if (playerCount < maxPlayers || playerPaidCount < maxPlayers) {
+      return { status: 'Équipe incomplète', theme: 'warning' }
+    } else {
+      return { status: 'Équipe complète', theme: 'success' }
+    }
   }
 
-  return { status: 'Équipe inscrite', theme: 'success' }
 }
