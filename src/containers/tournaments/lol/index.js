008be96c907bbbcd2c117a905fefef3270264614
Arnaud9145 <arnaud.dufour@utt.fr>
2018-10-17 02:41:47 +0200

    added infos for lol and ssbu

--- src/containers/tournaments/lol/index.js
+++ src/containers/tournaments/lol/index.js
@@ -10,33 +10,48 @@ const LOL = () => {
       <Category>League of Legends</Category>
     </div>
     <div className="a-tournament-content">
-      <p>Vous retrouverez sur cette page toutes les informations relatives aux tournois League of Legends (pro et amateur). <strong>Attention</strong>, ces informations pourront évoluer,
+      <p>Vous retrouverez sur cette page toutes les informations relatives aux tournois League of Legends (pro et amateur). <strong>Attention</strong>, ces informations pourront évoluer jusqu'au début du tournoi,
       tenez vous informé via nos réseaux (<a href="https://www.facebook.com/UTTArena/">Facebook</a>/<a href="https://twitter.com/UTTArena">Twitter</a>).</p>
       
       <Category>Informations générales</Category>
-      <p>L'inscription à l'UTT Arena est fixée à 15€ par joueur. La LAN se déroulera du Vendredi 7 Décembre 18h au Dimanche 9 Décembre à 18h <strong>sans interruption</strong>. 
-      Les tournois commenceront le samedi à 10h. Il sera mis à disposition une buvette, où vous pourrez manger en continu à prix réduit.</p>
-      
+      <p>L'inscription à l'UTT Arena est fixée à 15€ par joueur. La LAN se déroulera du Vendredi 7 Décembre 18h au Dimanche 9 Décembre jusqu'à 18h <strong>sans interruption</strong>. 
+      Les tournois commenceront le Samedi à 10h. Il sera mis à disposition une buvette, avec de la nourriture en continu à prix réduit.</p>
+      <p>La participation aux tournois est ouverte à toutes personnes physiques ayant au moins 15 ans révolus. Pour valider leur participation, 
+      les joueurs mineurs doivent y être autorisés par leurs représentants légaux. Les documents officiels justifiant de l’identité des 
+      représentants légaux, du joueur mineur et l’autorisation parentale doivent être à porté du joueur lors de tout l’évènement et prêt 
+      à être présentés à tout moment. Un contrôle sera effectué à l'arrivée.</p>
+
       <Category>Tournoi Libre</Category>
       <p>En parallèle des tournois "Spotlights" (LoL, Hearthstone, CS:GO, Fortnite, SSBU) se déroulera un tournoi libre. Celui-ci sera composé de différents mini-tournois, sur des jeux divers et variés.
       <strong>Tout joueur de tournoi Spotlight éliminé rejoindra automatiquement le tournoi Libre.</strong> Il pourra ainsi profiter d'animations même s'il n'est plus dans la compétition.</p>
 
-      <Category>Règlements</Category>
+      <Category>Règlement</Category>
       <p>En participant à cette compétition, les joueurs acceptent, sans réticence ou interprétation, de respecter le règlement.
       Il est à noter que celui-ci est susceptible d’évoluer durant la LAN en fonction des besoins (retard, imprécision...). </p>
       <p>Par ailleurs, en cas de divergence d’interprétation de ce document, l’avis des administrateurs du tournoi a préséance sur celui des joueurs. Les joueurs doivent respecter les horaires
        donnés par les organisateurs et s’assurer d’être disponibles lorsqu’ils seront appelés avant le début du tournoi ainsi qu’à chaque fois que cela sera nécessaire, sous peine de disqualification. </p>
       
-      <h2>Prérequis</h2>
+      
+
+      <h2>Règles générales</h2>
       <p>Le tournoi est organisé sur les serveurs officiels de <strong>l’Europe de l’Ouest.</strong>  Les participants doivent donc disposer d’un compte d’invocateur et d’un client du jeu à jour pour pouvoir participer à l’évènement.
       <strong> Les tournois commencent le samedi à 10h</strong>.</p>
-
-      <h2>Règles générales du jeu</h2>
       <p><strong>Mode :</strong> partie personnalisée « Tournament Draft Mode »</p>
       <p><strong>Map :</strong> faille de l'invocateur (5c5)</p>
       <p><strong>Sélection des champions :</strong> avant le début du match, une équipe sera tirée au sort. L’équipe en question se retrouvera à gauche lors de la constitution du match. Les Bans et les picks devront respecter la procédure de draft habituelle.</p>
       <p><strong>ATTENTION :</strong> les champions sortis 2 semaines avant le tournoi ne seront pas acceptés lors des matchs. Les équipes n’auront pas besoin de ban le ou les champions récemment sortis mais ne pourront pas le/les jouer.</p>
 
+      <p>Cette année, deux tournois seront organisés séparément, un <strong>tournoi amateur</strong> qui comportera 24 équipes et 
+      un tournoi <strong>professionnel </strong>
+      de 8 équipes. Lors de l’inscription, vous décidez dans quel tournoi vous participerez. Toutefois, les organisateurs se réservent le 
+      droit de vous affecter à l’autre tournoi s’ils jugent un écart de niveau trop important par rapport aux autres participants. Les pseudos
+       indiqués lors de l’inscription devront être identiques aux pseudos en jeu. Si des joueurs sont suspectés d’être des smurfs, leur équipe
+        peut se faire disqualifier par les organisateurs. De plus, seuls les joueurs ayant atteint le<strong> niveau 30</strong> peuvent participer.</p>
+
+
+      <p>Les récompenses du tournoi amateurs seront des lots, tandis que les récompenses du tournoi professionnel correspondent à un cashprize.</p>
+      <p>Pour le bon déroulement du tournoi, tout retard dépassant vingt minutes entraînera la perte immédiate de la partie. 
+      Si plusieurs retards sont cumulés, les arbitres se réservent le droit de disqualifier l’équipe du tournoi.</p>
       <h2>Déroulement d'un match</h2>
       <p><strong>Avant le match :</strong> si une équipe ne se présente pas dans le temps imparti, elle sera considérée comme déclarant forfait.</p>
       <p><strong>Capitaine :</strong> pour créer une équipe sur le site, un joueur doit jouer le rôle de capitaine d'équipe, et les autres joueurs rejoindront son équipe. Ce capitaine
@@ -47,8 +62,40 @@ const LOL = () => {
       un joueur possède des problèmes à répétition dûs à sa configuration, il pourra être disqualifié. Son équipe devra alors chercher un remplaçant (parmi des joueurs du tournoi libre) ou abandonner, à moins qu'il ne trouve du matériel de remplacement.</p>
       <p><strong>Retard :</strong> tout retard de plus de 20 minutes entraînera le forfait automatique d'une partie.</p>
       
-      <h2>Format du tournoi (à venir)</h2>
+      <h2>Format du tournoi</h2>
+      <h3>Tournoi amateur </h3>
 
+      <p>Le tournoi se présentera sous la forme d’un système suisse:</p>
+      <ul>
+        <li>1er tour : Chaque équipe affronte un adversaire choisi par les organisateurs en 
+        fonction d’un classement préétabli et basé sur le niveau global de chaque équipe. </li>
+        <li>2ème tour : Les équipes gagnantes se rencontrent et de même pour les équipes perdantes. 
+        Si le nombre d’équipe est impaire, une des équipes gagnante rencontrera une équipe qui a perdu.</li>
+        <li>Les arbitres décideront des rencontres.</li>
+      </ul>
+      
+      <p>Jusqu’au 6ème tour inclus : Le principe reste le même. Une équipe qui a N victoires jouera contre une équipe à N victoires.</p>
+      
+      <p><strong>Remarque</strong> : Deux équipes ne peuvent pas se rencontrer plusieurs fois lors de la phase Suisse.</p>
+      
+      <p>7ème tour : Les 4 équipes cumulant le plus de victoires s’affronteront en demie finale. Les rencontres seront tirés aléatoirement. 
+      Si deux équipes ont le même nombre de victoires, les arbitres trancheront.</p>
+      
+      <p>Finale et petite finale  : Les perdants de chaque demie-finale s’affrontent en petite finale, et les gagnants, en finale. Ces deux rencontres se dérouleront en 2 matchs gagnants (BO3).</p>
+      
+      
+      
+      <h3>Tournoi Pro</h3>
+      
+      <p>Les huit équipes du tournoi professionnel seront réparties aléatoirement en deux poules. Les matchs de poules se feront en BO1.</p>
+      <p>Toutes les équipes seront réparties dans un arbre à double élimination (avec loser bracket). </p>
+      <p>Tous les matchs se feront en BO1 exceptées
+       la finale du winner bracket et la grande finale qui se joueront en BO3. Le gagnant du winner bracket se voit accorder une victoire
+        d’avance dans le BO3 de la grande finale.
+      </p>
+
+      <p>Les joueurs peuvent accéder à l'arbre du tournoi à tout moment sur une télévision pendant l'événement
+       ainsi que sur l'intranet, disponible sur le lien suivant : (à venir)</p>
       <h2>Règles de bonne conduite</h2>
       <p>Tous les participants sont invités à se comporter de façon respectueuse envers les autres participants. Une équipe peut être réprimandée et recevoir un avertissement si un de ses joueurs :</p>
       <ul>
@@ -59,16 +106,16 @@ const LOL = () => {
       </ul>
       <p>Un manquement à ces règles pourra entraîner <strong>un avertissement, la perte d'une partie ou d'un match ou encore la disqualification de l’équipe.</strong> </p>
       
-      <h2>Finales sur scène</h2>
+      <h2>Matchs castés et finales sur scènes</h2>
       <p>Cette année, les finalistes auront l'occasion de pouvoir jouer les finales sur la scène de l'événement, devant le public du festival.
-      C'est une occasion unique de pouvoir montrer son skill devant tout le monde, mais quelques règles s'imposent.</p>
+      Seulement quelques règles s'imposent.</p>
       <h3>Obligation</h3>
       <p>Jouer sur scène est <strong>obligatoire</strong>, tout refus entraînera la disqualification de l'équipe.</p>
       <h3>Matériel</h3>
       <p>Il vous sera mis à disposition sur scène du matériel dernière génération par notre partenaire <strong>Scoup Esport</strong> (clavier, souris, casque, tour, écran, chaise). 
       Seulement, nous sommes conscients que vous êtes habitués à utiliser votre matériel pour jouer.
       Ainsi, il vous sera possible d'emmener sur scène votre clavier et votre souris. Si vous souhaitez utiliser votre écran, pc ou tout autre matériel, il faudra que vous le fassiez 
-      valider par le staff avant, et que cela ne mette pas le début du match en retard. N'hésitez pas à demander l'aide du staff <strong>à l'avance</strong>. Attention, <strong>
+      valider par le staff avant, et que cela ne mette pas le match en retard. N'hésitez pas à demander l'aide du staff <strong>à l'avance</strong>. Attention, <strong>
       pour le casque, il faudra qu'il soit anti-bruit et que le micro soit directionnel</strong>. Sinon vous devrez utiliser le casque que nous fournirons.</p>
       <p><strong>Tout retard de plus de 20 minutes dû à un joueur qui prendra trop de temps à s'installer entraînera le forfait automatique d'une partie.</strong></p>
       <h3>Configuration</h3>
