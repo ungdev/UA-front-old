d009c7edf8c12c6ef386268aac6f3b05fdcc971d
Arnaud9145 <arnaud.dufour@utt.fr>
2018-11-07 10:55:26 +0100

    handled emty teams variable

--- src/containers/dashboard/viewParticipants/index.js
+++ src/containers/dashboard/viewParticipants/index.js
@@ -65,7 +65,7 @@ class Participants extends React.Component {
 
 const mapStateToProps = state => ({
   spotlights: state.spotlights.spotlights,
-  teams: state.teams.teams
+  teams: state.teams.teams || [],
 })
 
 export default connect(mapStateToProps)(Participants)
