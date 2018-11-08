3fee719bb7cba9d3defd06d502763625140a2523
Lyght <valentinpeltier@yahoo.fr>
2018-11-08 11:13:27 +0100

    Add keyboard navigation

--- src/containers/gallery/components/imageView/index.js
+++ src/containers/gallery/components/imageView/index.js
@@ -12,10 +12,44 @@ class ImageView extends React.Component {
 
     this.preventClose = false
 
+    this.keydownHandle = this.keydownHandle.bind(this)
     this.leftArrowClick = this.leftArrowClick.bind(this)
     this.rightArrowClick = this.rightArrowClick.bind(this)
     this.imageClick = this.imageClick.bind(this)
     this.containerClick = this.containerClick.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.keydownHandle)
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps !== this.props) {
+      this.setState({
+        index: this.props.index
+      })
+    }
+  }
+
+  keydownHandle(e) {
+    if(this.state.index !== null) {
+      switch(e.keyCode) {
+        // Escape key
+        case 27:
+          this.setState({
+            index: null
+          })
+          return
+        // Left arrow key
+        case 37:
+          this.leftArrowClick()
+          return
+        // Right arrow key
+        case 39:
+          this.rightArrowClick()
+          return
+      }
+    }
   }
 
   leftArrowClick() {
@@ -36,14 +70,6 @@ class ImageView extends React.Component {
     }
 
     this.preventClose = true
-  }
-
-  componentDidUpdate(prevProps) {
-    if(prevProps !== this.props) {
-      this.setState({
-        index: this.props.index
-      })
-    }
   }
 
   imageClick() {
