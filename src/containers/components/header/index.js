7365239c60583497229178e18cc9886a24f78d7f
Valentin Peltier <valentinpeltier@yahoo.fr>
2019-03-15 11:02:40 +0100

    Revert "Add hover effect to buttons"
    This reverts commit 77ca54c0a73806bf315ad579cf8ba3b2c47c22c4.

--- src/containers/components/header/index.js
+++ src/containers/components/header/index.js
@@ -74,7 +74,7 @@ class Header extends React.Component {
         <nav className="a-intro-header__nav__mobile">
           <div className="a-intro-header__nav__mobile__topbar">
             <div style={{ paddingBottom: '3px' }}>
-              <Button onClick={this.toggleMobileMenu} className="no-animation">
+              <Button onClick={this.toggleMobileMenu}>
                 <div className={"a-intro-header__hamburger" + (this.state.mobileMenu ? " active" : "")}>
                   <div className="a-intro-header__hamburger__segment"></div>
                   <div className="a-intro-header__hamburger__segment"></div>
